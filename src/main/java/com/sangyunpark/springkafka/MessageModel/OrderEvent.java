4d125e3474906d585f97ec0c7158d1bbf4bd2d28
sangyunpark99 <psangyun5108@gmail.com>
2025-03-20 14:55:46 +0900

    feat: OrderEvent 메시지 작성

---
+++ src/main/java/com/sangyunpark/springkafka/MessageModel/OrderEvent.java
@@ -0,0 +1,30 @@
+package com.sangyunpark.springkafka.MessageModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+
+    private String orderId;
+    private String customerId;
+    private List<OrderItem> items;
+    private BigDecimal totalAmount;
+    private LocalDateTime orderTime;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OrderItem {
+        private String productId;
+        private int quantity;
+        private BigDecimal price;
+    }
+}

ecf31ae9c9d3a9aa639345764522e951d529b88e
sangyunpark99 <psangyun5108@gmail.com>
2025-03-22 23:53:56 +0900

    test: test코드 구현

--- src/test/java/com/sangyunpark/springkafka/producer/OrderProducerTest.java
+++ src/test/java/com/sangyunpark/springkafka/producer/OrderProducerTest.java
@@ -2,8 +2,11 @@ package com.sangyunpark.springkafka.producer;
 
 import com.sangyunpark.springkafka.model.OrderEvent;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -11,9 +14,13 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
+import java.math.BigDecimal;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 public class OrderProducerTest {
@@ -25,6 +32,9 @@ public class OrderProducerTest {
     private KafkaTemplate<String, OrderEvent> kafkaTemplate;
 
     private Consumer<String, OrderEvent> consumer;
+
+
+    // 테스트 환경에서는 항상 처음부터 메시지를 읽게 한다.
 
     @BeforeEach
     void setUp() {
@@ -40,8 +50,33 @@ public class OrderProducerTest {
                 new JsonDeserializer<>(OrderEvent.class)
         ).createConsumer();
 
-        consumer.subscribe(List.of("orders"));
-        consumer.poll(Duration.ofMillis(100));
-        consumer.seekToBeginning(consumer.assignment());
+        // 카프카에서 메시지를 생산하고, 이 메시지가 잘 전송되었는지 테스트용 컨슈머로 확인하는 테스트 환경 설정 코드
+        consumer.subscribe(List.of("orders")); // 테스트용 토픽
+        consumer.poll(Duration.ofMillis(100)); // 카프카 컨슈머가 실제 메시지를 읽어오는 작업
+        consumer.seekToBeginning(consumer.assignment()); // 오프셋 가장 처음 위치로 이동
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
+    @Test
+    void testSendOrder() {
+        //given
+        OrderEvent order = createTestOrder();
+
+        // when
+        producer.sendOrder(order);
+
+        // then
+        ConsumerRecord<String, OrderEvent> record = KafkaTestUtils.getSingleRecord(consumer, "orders");
+        assertThat(record).isNotNull();
+        assertThat(record.value().getOrderId()).isEqualTo(order.getOrderId());
+    }
+
+    private OrderEvent createTestOrder() {
+        List<OrderEvent.OrderItem> items = List.of(new OrderEvent.OrderItem("prod-1", 2, BigDecimal.valueOf(20.00)));
+        return new OrderEvent("order-123", "cust-456", items, BigDecimal.valueOf(40.00), LocalDateTime.now());
     }
 }
